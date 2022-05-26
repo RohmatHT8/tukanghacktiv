e9e4aade2b97cbf6e4ee013bb9c86d5f41703e40
noruhm <henryhrty@gmail.com>
2022-05-26 18:58:29 +0700

    tambahan

--- script.js
+++ script.js
@@ -120,6 +120,7 @@ const tukang = {
             img : 'img/poto.jpg'}]
 }
 
+let keranjang ={}
 function render(data){
     let result= data
     let temp=''
@@ -144,3 +145,80 @@ function render(data){
         }
     document.getElementById('content').innerHTML= temp
 }
+function tambahkeranjang(arr){
+    if(!keranjang.hasOwnProperty(arr[0])){
+        keranjang[arr[0]] = {
+            price: arr[1],
+            img: arr[2],
+            qty: 1
+        }
+    }
+    togglecart();
+    cekcount()
+}
+
+function cekcount(){
+    let counter =0
+    for (let i in keranjang)
+    {
+        counter++
+    }
+    document.getElementById('cartcount').innerHTML=counter
+}
+function togglecart(){
+    let cart = document.getElementById('carttable')
+    let header = '<tr><thead><td>gambar</td><td>Nama Orang</td><td>Harga</td><td>Total Harga</td><td>Action</td></thead></tr>'
+    let barang =''
+    for (let i in keranjang){
+        let total = keranjang[i].price*keranjang[i].qty
+        barang+=`<tr><td><img src='${keranjang[i].img}'></td><td>${i}</td><td>${keranjang[i].price}</td><td>${keranjang[i].qty}</td><td>${total}</td><td><button class="btn btn-danger" onclick="removeitem('${i}')">Remove</button></td></tr>`
+    }
+    cart.innerHTML=header+barang
+}
+function removeitem(item){
+    delete keranjang[item]
+    cekcount()
+    togglecart()
+}
+let modal = document.getElementById('myModal')
+let btn = document.getElementById('myBtn')
+let span = document.getElementsByClassName('close')[0]
+
+btn.onclick = function()
+{
+    modal.style.display='block';
+}
+span.onclick = function()
+{
+    modal.style.display='none'
+}
+window.onclick = function(event)
+{
+    if(event.target === modal){
+        modal.style.display='none'
+    }
+}
+
+
+function plusqty(item){
+    keranjang[item].qty++
+    cekcount()
+    togglecart()
+}
+
+function minusqty(item){
+    if(keranjang[item].qty>1){
+        keranjang[item].qty--
+    }
+    cekcount()
+    togglecart()
+}
+
+function checkout(){
+    keranjang = {}
+    alert("Check Out Berhasil")
+    cekcount()
+    togglecart()
+}
+
+    
